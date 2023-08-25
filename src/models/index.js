aa0431988fe34e57a8ba525a4e99b899a21a4ac4
SaeedKokash <saeedkokash@gmail.com>
2023-08-25 19:58:56 +0000

    added node_env

--- src/models/index.js
+++ src/models/index.js
@@ -1,29 +1,28 @@
-'use strict';
+"use strict";
 
-const { Sequelize } = require('sequelize');
-const dotenv = require('dotenv');
+const { Sequelize } = require("sequelize");
+const dotenv = require("dotenv");
 
 dotenv.config();
 const DATABASE_URL = process.env.DATABASE_URL;
 
-// for deployment
-const sequelizeOptions = {
+// for production
+if (process.env.NODE_ENV === "production") {
+  const sequelizeOptions = {
     dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false
-        }
-    }
-};
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  };
 
-const sequelize = new Sequelize(DATABASE_URL, sequelizeOptions);
-
+  const sequelize = new Sequelize(DATABASE_URL, sequelizeOptions);
+}
 
 // for development
-
-// const sequelizeOptions = {};
-// const sequelize = new Sequelize(DATABASE_URL, {});
-
-
-
+if (process.env.NODE_ENV === "development") {
+  const sequelizeOptions = {};
+  const sequelize = new Sequelize(DATABASE_URL, sequelizeOptions);
+}
 module.exports = { sequelize };
