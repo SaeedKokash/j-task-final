43aeab817d5475761a2afcd7c34053c941e7c5ec
SaeedKokash <saeedkokash@gmail.com>
2023-08-25 17:56:03 +0000

    fixed database

--- src/models/index.js
+++ src/models/index.js
@@ -6,6 +6,24 @@ const dotenv = require('dotenv');
 dotenv.config();
 const DATABASE_URL = process.env.DATABASE_URL;
 
-const sequelize = new Sequelize(DATABASE_URL, {});
+// for deployment
+const sequelizeOptions = {
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
+};
+
+const sequelize = new Sequelize(DATABASE_URL, sequelizeOptions);
+
+
+// for development
+
+// const sequelizeOptions = {};
+// const sequelize = new Sequelize(DATABASE_URL, {});
+
+
 
 module.exports = { sequelize };
